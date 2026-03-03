ba8c0832109fee7afbd1771b3542dbd0bc7692ae
Iman Lzr <imanelaazraoui@gmail.com>
2026-03-03 22:07:50 +0000

    change

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
-def say_hello():
-    print("Hello DevOps")
-
-
- def say_hello():
-    print("Hello DevOps")
-
-
- 
-
-
-say_hello()
+def test():
+    x = 10
+    y = 20
+    print("Demo code")
+    if True:
+        print("Always true condition")
+    print(x)
+    print(y)
+def test_duplicate():
+    print("Demo code")   # Duplicate smell
+test()
+test_duplicate()
