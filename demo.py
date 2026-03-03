64a432cc38e3dd2ba4c911349daf647b5bcc5b95
Iman Lzr <imanelaazraoui@gmail.com>
2026-03-03 22:01:50 +0000

    change

--- demo.py
+++ demo.py
@@ -2,9 +2,11 @@ def say_hello():
     print("Hello DevOps")
 
 
-def say_hello_again():
-    print("Hello DevOps")   
+ def say_hello():
+    print("Hello DevOps")
+
+
+ 
 
 
 say_hello()
-say_hello_again()
